be6bdc8c5f5227c8aedacf7ec5cfd2ef50702f5a
simon@prclm <>
2022-11-05 00:10:16 +0100

    eslint + prettier code formating setup

---
+++ build-figma-plugin.manifest.js
@@ -0,0 +1,8 @@
+// build-figma-plugin.manifest.js
+
+// eslint-disable-next-line no-undef
+module.exports = function (manifest) {
+  return {
+    ...manifest,
+  }
+}
---
+++ build-figma-plugin.ui.js
@@ -0,0 +1,11 @@
+// build-figma-plugin.ui.js
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const eslint = require('esbuild-plugin-eslint')
+
+// eslint-disable-next-line no-undef
+module.exports = function (buildOptions) {
+  return {
+    ...buildOptions,
+    plugins: [...buildOptions.plugins, eslint()],
+  }
+}
